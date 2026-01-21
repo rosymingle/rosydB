3394e721b663d75b731f47c5f2b478b3e1fa80d6
rosymingle <rosymingle@gmail.com>
2026-01-21 13:34:30 +1100

    Delete js directory

--- js/showthething.js
+++
@@ -1,8 +0,0 @@
-$(document).on("ready", function () {
-	/* Rory's ghetto accordion (which is actually nicer than the plugin one) */
-	$('.showthething').click(function() {
-	  $(this).parent().find('.thething').slideToggle('fast');
-	  $(this).find('.icon').toggleClass('flipthething');
-	  return false
-	});
-});
